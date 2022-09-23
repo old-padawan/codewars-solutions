95bf5e51e4b8f99c7776173b9202765287b6864c
Anatolii Horobets <104827011+cheapsitestudio@users.noreply.github.com>
2022-09-24 01:32:03 +0300

    Create сats-and-shelves.js

---
+++ сats-and-shelves.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/62c93765cef6f10030dfa92b
+
+function solution(start, finish) {
+  const distanceForJumping = finish - start;
+  const oneJump = 3;
+  return Math.floor(distanceForJumping / oneJump + distanceForJumping % oneJump);  
+}
