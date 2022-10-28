b4b98d1373306cb22463211cc60cef4ba85e113f
Anatolii Horobets <admin@cheapsite.studio>
2022-10-29 00:27:13 +0300

    x marks the spot

--- 7kyu/x-marks-the-spot.js
+++ 7kyu/x-marks-the-spot.js
@@ -1,12 +1,12 @@
 const xMarksTheSpot = (input) => {
-    const searchX = [...input].join("").match(/x/g);
-    if (!/x/g.test([...input].join("")) || searchX.length > 1) {
+    const searchX = [...input].join('').match(/x/g);
+    if (!/x/g.test([...input].join('')) || searchX.length > 1) {
       console.log(false);
       return [];
     } else {
       for (let i = 0; i < input.length; i++) {
-        if (input[i].includes("x")) {
-          return [i, input[i].indexOf("x")];
+        if (input[i].includes('x')) {
+          return [i, input[i].indexOf('x')];
         }
       }
     }
