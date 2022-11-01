c07e58835c6806c87d8b1caf82e95f3e489f061b
Anatolii Horobets <104827011+cheapsitestudio@users.noreply.github.com>
2022-11-01 18:57:10 +0200

    Update smallest-value-of-an-array.js

--- smallest-value-of-an-array.js
+++ smallest-value-of-an-array.js
@@ -1,5 +1,7 @@
 // https://www.codewars.com/kata/544a54fd18b8e06d240005c0/train/javascript
 
 function min(arr, toReturn) {
-  return toReturn === 'index' ? arr.indexOf(Math.min(...arr)) : Math.min(...arr);
+  return toReturn === "index"
+    ? arr.indexOf(Math.min(...arr))
+    : Math.min(...arr);
 }
